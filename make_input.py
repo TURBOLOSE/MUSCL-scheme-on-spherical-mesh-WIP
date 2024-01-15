fixed sign in flux

make_input.py
@@ -12,20 +12,20 @@ def make_input_4(): #no energy as separate variable
 
     l=[]
 
-    omega=np.array([0,0,3])
+    omega=np.array([0,0,2])
     theta_face_centers=np.arccos(face_centers[:,2])
 
 
-    rho=np.ones(N)
-    #rho=np.exp( (np.linalg.norm(face_centers[0])**2)*(np.linalg.norm(omega)**2)/1.4*np.sin(np.pi-theta_face_centers)**3)
+    #rho=np.ones(N)
+    rho=np.exp( (np.linalg.norm(omega)**2)*np.sin(np.pi-theta_face_centers)**3)
 
 
     for face_num, R in enumerate(face_centers):
         #if( R[2] >=0):
-        #    omega=np.array([0,0,1])
+        #    omega=np.array([0,0,20])
         #else:
-        #    omega=np.array([0,0,-1])
-        l.append(rho[face_num]*np.cross(R,np.cross(omega,R)))
+        #    omega=np.array([0,0,-20])
+        l.append(rho[face_num]*np.cross(R,np.cross(omega,R))/np.linalg.norm(R))
     l=np.array(l)
 
     pd.DataFrame(data=np.array([rho, l[:,0],l[:,1],l[:,2]]).transpose()).to_csv('input/input.dat',index=False, sep=' ', header=False)
@@ -40,11 +40,13 @@ def make_input_5():  #adds energy
     face_centers=np.array(face_centers)
 
     rho=np.ones(N)
+
+
     p=np.ones(N)*0.1
     l=[]
 
 
-    omega=np.array([0,0,1])
+    omega=np.array([0,0,0])
 
     for face_num, R in enumerate(face_centers):
         #if( R[2] >=0):
@@ -62,7 +64,7 @@ def make_input_5():  #adds energy
 
 
 
-make_input_5()
+make_input_4()
 
 
 

plots.py
@@ -134,12 +134,15 @@ axis_dist=[]
 # fig=px.scatter(x=axis_dist, y=rho,  labels={"x": "R", "y":"rho"})
 # fig.show()
 
-# fig=px.scatter(x=theta_fc, y=rho,  labels={"x": "theta", "y":"rho"})
-# fig.show()
 
 
-# #fig=px.scatter(x=theta_fc, y=np.exp(5./7*np.sin(np.pi/2+theta_fc)**3),  labels={"x": "theta", "y":"rho"})
-# #fig.show()
+rho=np.array(data_rho.loc[maxstep-100,1:len(faces)])
+fig=px.scatter(x=theta_fc, y=rho,  labels={"x": "theta", "y":"rho"})
+fig.show()
+
+
+fig=px.scatter(x=theta_fc, y=np.exp(4*np.sin(np.pi-theta_fc)**3)/55,  labels={"x": "theta", "y":"rho"})
+fig.show()
 
 
 
@@ -163,10 +166,12 @@ axis_dist=[]
 
 
 
-#data=pd.read_table('errors.txt', header=None, delimiter=r"\s+")
+data=pd.read_table('errors_p.txt', header=None, delimiter=r"\s+")
 
-#fig=px.scatter(x=data[0], y=data[1],  labels={"x": "t", "y":"relative rho error"})
-#fig.update_layout(font=dict(size=30))
-#fig.show()
+T_rot=2*np.pi/8
+
+fig=px.scatter(x=data[0]/T_rot, y=data[1],  labels={"x": "N_of_rotations", "y":"relative rho error"}, log_y=True)
+fig.update_layout(font=dict(size=30))
+fig.show()
 
 